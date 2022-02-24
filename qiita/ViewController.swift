4a2f44efef87da4a3aedfed82d8532734554525e
masatakasumida <96749001+masatakasumida@users.noreply.github.com>
2022-02-24 15:20:13 +0900

    Revert "UITableViewの実装を修正"

--- qiita/ViewController.swift
+++ qiita/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var itemArray = ["cell 1","cell 2","cell 3","cell 4","cell 5","cell 6","cell 7","cell 8","cell 9"]
+    var itemArray = ["cell 1","cell 2","cell 3"]
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -17,44 +17,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let tableView = UITableView(frame: self.view.bounds, style: .plain)
-        //tableViewの設定を記述
+//tableViewの設定を記述
         tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
         tableView.delegate = self
         tableView.dataSource = self
         view.addSubview(tableView)
-    }
+       }
 }
 
 extension ViewController: UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        itemArray.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //tableViewの内容を指定
+//tableViewの内容を指定
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell",for: indexPath) as! TableViewCell
-        let cellTitle = itemArray[indexPath.row]
-        cell.configure(title: cellTitle)
-        cell.indexPathRow = indexPath.row
-        //忘れやすいので注意
+//忘れやすいので注意
         cell.delegate = self
         return cell
     }
 }
 extension ViewController: TableViewCellDelegate {
     
-    func deleteButton(indexPathRow: Int) {
-        let alert: UIAlertController = UIAlertController(title: "注意", message: "削除してもいいですか？", preferredStyle: .alert)
-        let canselAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: .cancel) { (UIAlertAction) in
-            print("キャンセル")
-        }
-        let okAction: UIAlertAction = UIAlertAction(title: "削除", style: .destructive) { (UIAlertAction) in
-            self.itemArray.remove(at: indexPathRow)
+    func deleteButton() {
+    let alert: UIAlertController = UIAlertController(title: "注意", message: "削除してもいいですか？", preferredStyle: .alert)
+    let canselAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: .cancel) { (UIAlertAction) in
+        print("キャンセル")
+    }
+    let okAction: UIAlertAction = UIAlertAction(title: "削除", style: .destructive) { (UIAlertAction) in
+        self.tableView.beginUpdates()
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "customCell",for: indexPath) as! TableViewCell
+            self.itemArray.remove(at: indexPath.row)
+            
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            
             self.tableView.reloadData()
+            self.tableView.endUpdates()
+            return cell
         }
-        alert.addAction(okAction)
-        alert.addAction(canselAction)
-        present(alert, animated: true, completion: nil)
+        print(self.itemArray)
+}
+    alert.addAction(okAction)
+    alert.addAction(canselAction)
+    present(alert, animated: true, completion: nil)
     }
 }
