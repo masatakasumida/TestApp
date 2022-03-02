7fcc6fa33260ffaf14beec06e35bc057cc65b4d3
masatakasumida <masasumi0327@gmail.com>
2022-03-03 05:48:27 +0900

    cellの削除機能を実装

--- qiita/TableViewCell.swift
+++ qiita/TableViewCell.swift
@@ -8,24 +8,26 @@
 import UIKit
 
 protocol TableViewCellDelegate {
-    func deleteButton()
+    func deleteButton(indexPathRow: Int)
 }
 
 class TableViewCell: UITableViewCell {
     
     var delegate: TableViewCellDelegate?
-
+    var indexPathRow = 0
+    
+    @IBOutlet weak var content: UILabel!
     @IBAction func deleteButton(_ sender: Any) {
-        delegate?.deleteButton()
+        delegate?.deleteButton(indexPathRow: indexPathRow)
     }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
